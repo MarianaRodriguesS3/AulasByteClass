7ddf532ddc4a32f76b0f00087d357e3fdc51cb19
Mariana Rodrigues <166456130+MarianaRodriguesS3@users.noreply.github.com>
2025-06-20 10:12:19 -0300

    adicionando as operações de multiplicação, divisão e porcentagem

--- aula3/index.js
+++ aula3/index.js
@@ -1,12 +1,13 @@
 var posicao1 = 2
 var posicao2 = 3
-
+// soma
 function soma(a, b) {
-    console.log(a + b)
+    //console.log(a + b)
     return a + b
 }
 soma(posicao1, posicao2)
 
+// subtração
 function subtrai(a, b) {
     return a - b
 }
@@ -14,16 +15,51 @@ subtrai(posicao1, posicao2)
 
 // TODO: fazer operações /, * e %
 // TODO: tratar divisão por zero
-function operacao(n1,n2,op){
-    if (op == '+')
-        return soma(n1,n2);
-    else if (op == '-')
-        return subtrai(n1,n2);
+
+//multiplicação
+function multiplica(a, b) {
+    return a * b;
+}
+multiplica(posicao1, posicao2);
+
+//divisão
+function divide(a, b) {
+    if (b == 0) {
+        return "Erro: divisão por zero";
+    } return a / b;
+}
+divide(posicao1, posicao2);
+
+//porcentagem
+function percentual(a, b) {
+    return (a / 100) * b;
+}
+percentual(posicao1, posicao2);
+
+function operacao(n1, n2, op) {
+    if (op == '+') 
+        return soma(n1, n2);
+    else if (op == '-') 
+        return subtrai(n1, n2);
+    else if (op == '*') 
+        return multiplica(n1, n2);
+    else if (op == '/') 
+        return divide(n1, n2);
+    else if (op == '%') 
+        return percentual(n1, n2);
     else 
-        return 'operação inválida'
+    return 'operação inválida';
 }
 
-operacao(20,10,'+')
+//testando
+console.log("Operação +:", operacao(20, 10, '+'));
+console.log("Operação -:", operacao(20, 10, '-'));
+console.log("Operação *:", operacao(20, 10, '*'));
+console.log("Operação /:", operacao(20, 10, '/'));
+console.log("Operação / com zero:", operacao(20, 0, '/'));
+console.log("Operação %:", operacao(20, 10, '%'));
+console.log("Operação % com zero:", operacao(20, 0, '%'));
+console.log("Operação inválida:", operacao(20, 10, '^'));
 
 var cadastro = {
     nome: "Mari",
