1fb45aef2643b8fee0db5aafb07901b7fa6a340e
MarianaRodriguesS3 <marianamanson@hotmail.com>
2025-07-18 21:48:29 -0300

    js calculadora

--- aula10/calculadora.js
+++ aula10/calculadora.js
@@ -16,8 +16,38 @@ function divide(a, b) {
     return a / b;
 }
 
-function adicionarNoVisor(valor) {
-    document.getElementById('visor').value += valor;
-    //document.getElementById('visor').value = document.getElementById('visor').value + valor;
+// Decide a operação com base no operador
+function operacao(n1, n2, op) {
+    switch (op) {
+        case '+': return soma(n1, n2);
+        case '-': return subtrai(n1, n2);
+        case '*': return multiplica(n1, n2);
+        case '/': return divide(n1, n2);
+        default: return 'operação inválida';
+    }
+}
 
+const visor = document.getElementById('visor');
+
+function adicionarNoVisor(valor) {
+    visor.value += valor;
+}
+
+function limparVisor() {
+    visor.value = '';
+}
+
+function calcularResultado() {
+    const expressao = visor.value;
+    const operadores = ['+', '-', '*', '/'];
+
+    const operador = operadores.find(op => expressao.includes(op));
+    if (!operador) return visor.value = "Expressão inválida";
+
+    const partes = expressao.split(operador);
+    if (partes.length !== 2) return visor.value = "Expressão inválida";
+
+    const [n1, n2] = partes.map(Number);
+
+    visor.value = operacao(n1, n2, operador);
 }
