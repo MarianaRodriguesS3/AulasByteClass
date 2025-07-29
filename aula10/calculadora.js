1e8eaa93825f88746c545d11780637676b60f8e6
MarianaRodriguesS3 <marianamanson@hotmail.com>
2025-07-29 17:01:03 -0300

    endereço site freecodecamp

--- aula10/calculadora.js
+++ aula10/calculadora.js
@@ -1,3 +1,5 @@
+//https://www.freecodecamp.org/portuguese/news/como-construir-uma-calculadora-html-do-zero-usando-javascript/
+
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator__keys")
 
