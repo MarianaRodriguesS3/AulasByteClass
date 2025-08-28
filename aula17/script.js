413930d0b91bebaffb97243025f578990e83b212
MarianaRodriguesS3 <marianamanson@hotmail.com>
2025-08-28 16:02:27 -0300

    continuação atividades aula17

--- aula17/script.js
+++ aula17/script.js
@@ -14,7 +14,7 @@ function calcularOperacoes(numeros) {
     console.log(`Soma: ${soma}`);
     console.log(`Subtração: ${subtracao}`);
     console.log(`Multiplicação: ${multiplicacao}`);
-    console.log(`Divisão: ${divisao}`);
+    console.log(`Divisão: ${divisao}\n`);
 }
 calcularOperacoes(numeros);
 
@@ -57,11 +57,11 @@ let num = [10, 20, 30];
 
 function verificarMaiorMenor(numeros) {
     if (numeros[0] > numeros[1] && numeros[0] > numeros[2]) {
-        console.log(`O número ${numeros[0]} é o maior`);
+        console.log(`\nO número ${numeros[0]} é o maior`);
     } else if (numeros[1] > numeros[0] && numeros[1] > numeros[2]) {
-        console.log(`O número ${numeros[1]} é o maior`);
+        console.log(`\nO número ${numeros[1]} é o maior`);
     } else {
-        console.log(`O número ${numeros[2]} é o maior`);
+        console.log(`\nO número ${numeros[2]} é o maior`);
     }
 
     if (numeros[0] < numeros[1] && numeros[0] < numeros[2]) {
@@ -73,3 +73,144 @@ function verificarMaiorMenor(numeros) {
     }
 }
 verificarMaiorMenor(num);
+
+// Mostre a tabuada de um numero informado pelo usuário (de 1 a 10).
+
+let tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function calcularTabuada(tabuada) {
+    for (let i = 0; i < tabuada.length; i++) {
+        console.log(`\nTabuada de ${tabuada[i]}:`);
+        for (let t = 1; t <= 10; t++) {
+            console.log(`${tabuada[i]} x ${t} = ${tabuada[i] * t}`);
+        }
+    }
+}
+calcularTabuada(tabuada);
+
+// Calcule a soma dos numeros de 1 a 100 usando um laço de repetição.
+
+function somadeUmaCem() {
+    let soma = 0;
+    for (let i = 1; i <= 100; i++) {
+        soma += i;
+    }
+    return soma;
+}
+
+let resultado = somadeUmaCem();
+console.log(`\nA soma de 1 a 100 é: ${resultado}`);
+
+// Peça um numero inteiro positivo e calcule o seu fatorial.
+
+function calcularFatorial(n) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+console.log(`\n5! = ${calcularFatorial(5)}`);
+
+// Faça um programa que leia vários numeros até que o usuário digite 0. No final, mostre a média deles.
+
+function calcularMediaAteZero(mediaNumeros) {
+    let soma = 0;
+    let quantidade = 0;
+
+    for (let i = 0; i < mediaNumeros.length; i++) {
+        if (mediaNumeros[i] === 0) {
+            break;  // Para o loop ao encontrar 0
+        }
+        soma += mediaNumeros[i];
+        quantidade++;
+    }
+    let media = soma / quantidade;
+    console.log(`\nA média dos números digitados é: ${media}`);
+    return media;
+
+}
+let mediaNumeros = [10, 20, 30, 0];
+calcularMediaAteZero(mediaNumeros);
+
+// Leia 10 numeros e armazene-os em um vetor. Depois mostre apenas os numeros pares.
+
+let seqNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function mostraNumPares(numeros) {
+
+    let pares = [];
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            pares.push(numeros[i]);
+        }
+    }
+
+    return pares;
+}
+let numerosPares = mostraNumPares(seqNumeros);
+console.log(`\nNúmeros pares da sequência:`, numerosPares);
+
+// Leia uma matriz 3x3 e mostre a soma dos elementos da diagonal principal.
+
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+function somaDiagonalPrincipal(matriz) {
+    let soma = 0;
+    for (let i = 0; i < matriz.length; i++) {
+        soma += matriz[i][i]; // soma elementos da diagonal principal
+    }
+    return soma;
+}
+
+let resultadoSoma = somaDiagonalPrincipal(matriz);
+console.log(`\nA soma da diagonal principal é: ${resultadoSoma}`);
+
+// Crie um programa que leia 5 nomes e os imprima em ordem alfabética.
+
+let nomes = ["Lucas", "Ana", "Bruna", "Pedro", "Mariana"];
+
+function criaNovoVetorOrdenado(nomes) {
+
+    // Cria uma cópia ordenada do vetor sem modificar o vetor original
+    let nomesOrdenados = [...nomes].sort();
+
+    console.log("\nNovo vetor em ordem alfabética:\n", nomesOrdenados);
+}
+criaNovoVetorOrdenado(nomes);
+
+function exibeNomesOrdenados(nomes) {
+    // modifica o vetor original
+    nomes.sort();
+
+    console.log(`\nNomes em ordem alfabética:`);
+    for (let i = 0; i < nomes.length; i++) {
+        console.log(nomes[i]);
+    }
+}
+exibeNomesOrdenados(nomes);
+
+// Leia 2 vetores de 5 elementos e gere um terceiro vetor com a soma dos elementos de mesmo índice.
+
+let vetorA = [10, 20, 30, 40, 50];
+let vetorB = [10, 20, 30, 40, 50];
+
+function somaElementosMesmoIndice(vetorA, vetorB) {
+    let vetorSoma = [];
+
+    for (let i = 0; i < vetorA.length; i++) {
+        vetorSoma.push(vetorA[i] + vetorB[i]);
+    }
+
+    return vetorSoma;
+}
+let vetorC = somaElementosMesmoIndice(vetorA, vetorB);
+console.log(`\nlet vetorC =`, vetorC);
+
+// Crie uma função que receba um número e retorne True se ele for primo, senão False.
