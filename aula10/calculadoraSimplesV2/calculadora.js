7bd9c57b627f8e1ecd80a1badbed68f376f7d4a2
MarianaRodriguesS3 <marianamanson@hotmail.com>
2025-09-02 11:09:31 -0300

    atualização calculadora

--- aula10/calculadoraSimplesV2/calculadora.js
+++ aula10/calculadoraSimplesV2/calculadora.js
@@ -16,10 +16,6 @@ function divide(a, b) {
     return a / b;
 }
 
-function percentual(a, b) {
-    return (a / 100) * b;
-}
-
 // Decide a operação com base no operador
 function operacao(n1, n2, op) {
     switch (op) {
@@ -27,41 +23,36 @@ function operacao(n1, n2, op) {
         case '-': return subtrai(n1, n2);
         case '*': return multiplica(n1, n2);
         case '/': return divide(n1, n2);
-        case '%': return percentual(n1, n2);
         default: return 'operação inválida';
     }
 }
 
-// Adiciona valor ao visor
+const visor = document.getElementById('visor');
+
 function adicionarNoVisor(valor) {
-    document.getElementById('visor').value += valor;
-    //document.getElementById('visor').value = document.getElementById('visor').value + valor;
+    visor.value += valor;
 }
 
-// Limpa todo o visor (C)
 function limparVisor() {
-    document.getElementById('visor').value = '';
-}
-
-// CE: apaga a última entrada (número ou operador completo)
-function limparEntrada() {
-    let visor = document.getElementById('visor');
-    visor.value = visor.value.replace(/(\d+\.?\d*|\D)$/, '');
+    visor.value = '';
 }
 
 function calcularResultado() {
-    const visor = document.getElementById('visor');
     const expressao = visor.value;
-    const operadores = ['+', '-', '*', '/', '%'];
+    const operadores = ['+', '-', '*', '/'];
 
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
     const operador = operadores.find(op => expressao.includes(op));
     if (!operador) return visor.value = "Expressão inválida";
 
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
     const partes = expressao.split(operador);
     if (partes.length !== 2) return visor.value = "Expressão inválida";
 
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+    //https://www.w3schools.com/jsref/jsref_number.asp
     const [n1, n2] = partes.map(Number);
-    if (isNaN(n1) || isNaN(n2)) return visor.value = "Expressão inválida";
 
     visor.value = operacao(n1, n2, operador);
 }
